6605111aaafcf3983d7315e0c03ee98e0295b030
yanxl <1257885673@qq.com>
2019-11-14 10:44:10 +0800

    alg.js

--- alg.js
+++ alg.js
@@ -682,7 +682,9 @@ function HashTable() {
         this.count += 1
 
         if (this.count > this.limit * 0.75) {
-            this.resize(this.limit * 2)
+            let newSize = this.limit * 2
+            let newPrime = this.getPrime(newSize)
+            this.resize(newPrime)
         }
     }
     HashTable.prototype.get = function (key) {
@@ -716,7 +718,9 @@ function HashTable() {
                 bucket.splice(i, 1)
                 this.count--
                 if (this.limit > 7 && this.count < this.limit * 0.25) {
-                    this.resize(Math.floor(this.limit / 2))
+                    let newSize = Math.floor(this.limit / 2)
+                    let newPrime = tshi.getPrime(newSize)
+                    this.resize(newPrime)
                 }
                 return tuple[1]
             }
@@ -750,9 +754,26 @@ function HashTable() {
             }
         }
     }
+    HashTable.prototype.isPrime = function(num) {
+        let temp = parseInt(Math.sqrt(num))
+    
+        for (let i = 2; i <= temp; i++) {
+            if (num % i == 0) {
+                return false
+            }
+        }
+    
+        return true
+    }
+    HashTable.prototype.getPrime = function(num) {
+        while (!this.isPrime(num)) {
+            num += 1
+        }
+        return num
+    }
 }
 
-let hash = new HashTable()
+/* let hash = new HashTable()
 hash.put('abc',123)
 hash.put('cba',321)
 hash.put('nba',521)
@@ -764,4 +785,288 @@ console.log(hash.get('abc'))
 hash.remove('abc')
 console.log(hash.get('abc'))
 console.log(hash.isEmpty())
-console.log(hash.size())
+console.log(hash.size()) */
+
+function BinarySearchTree() {
+    function Node(key) {
+        this.key = key
+        this.left = null
+        this.right = null
+    }
+    this.root = null
+
+    BinarySearchTree.prototype.insert = function (key) {
+        let newNode = new Node(key)
+        if (this.root == null) {
+            this.root = newNode
+        } else {
+            this.insertNode(this.root, newNode)
+        }
+    }
+
+    BinarySearchTree.prototype.insertNode = function (node, newNode) {
+        if (newNode.key < node.key) {
+            if (node.left == null) {
+                node.left = newNode
+            } else {
+                this.insertNode(node.left, newNode)
+            }
+        } else {
+            if (node.right == null) {
+                node.right = newNode
+            } else {
+                this.insertNode(node.right, newNode)
+            }
+        }
+    }
+
+    // 先序遍历
+    BinarySearchTree.prototype.preOrderTraversal = function (handler) {
+        this.preOrderTraversalNode(this.root, handler)
+    }
+    BinarySearchTree.prototype.preOrderTraversalNode = function (node, handler) {
+        if (node != null) {
+            handler(node.key)
+            this.preOrderTraversalNode(node.left, handler)
+            this.preOrderTraversalNode(node.right, handler)
+        }
+    }
+
+    // 中序遍历
+    BinarySearchTree.prototype.midOrderTraversal = function (handler) {
+        this.midOrderTraversalNode(this.root, handler)
+    }
+    BinarySearchTree.prototype.midOrderTraversalNode = function (node, handler) {
+        if (node != null) {
+            this.midOrderTraversalNode(node.left, handler)
+            handler(node.key)
+            this.midOrderTraversalNode(node.right, handler)
+        }
+    }
+
+    // 后序遍历
+    BinarySearchTree.prototype.postOrderTraversal = function (handler) {
+        this.postOrderTraversalNode(this.root, handler)
+    }
+    BinarySearchTree.prototype.postOrderTraversalNode = function (node, handler) {
+        if (node != null) {
+            this.postOrderTraversalNode(node.left, handler)
+            this.postOrderTraversalNode(node.right, handler)
+            handler(node.key)
+        }
+    }
+    
+    BinarySearchTree.prototype.min = function () {
+        let node = this.root
+        if (node == null) return
+        while (node.left) {
+            node = node.left
+        }
+        return node.key
+    }
+    
+    BinarySearchTree.prototype.max = function () {
+        let node = this.root
+        if (node == null) return
+        while (node.right) {
+            node = node.right
+        }
+        return node.key
+    }
+    // 递归实现查找
+    // BinarySearchTree.prototype.search = function (key) {
+    //     this.searchNode(this.root, key)
+    // }
+    // BinarySearchTree.prototype.searchNode = function (node, key) {
+    //     if (node == null) return false
+    //     if (node.key > key) {
+    //         return this.searchNode(node.left, key)
+    //     } else if (node.key < key) {
+    //         return this.searchNode(node.right, key)
+    //     } else {
+    //         return true
+    //     }
+    // }
+
+    // 非递归实现查找
+    BinarySearchTree.prototype.search = function (key) {
+        let node = this.root
+
+        while(node != null) {
+            if (key < node.key) {
+                node = node.left
+            } else if (key > node.key) {
+                node = node.right
+            } else {
+                return true
+            }
+        }
+
+        return false
+    }
+
+    BinarySearchTree.prototype.remove = function (key) {
+        let current = this.root
+        let parent = null
+        let isLeftChild = true
+
+        // 寻找删除节点
+        while (current.key != key) {
+            if (key < current.key) {
+                isLeftChild = true
+                current = current.left
+            } else {
+                isLeftChild = false
+                current = current.right
+            }
+
+            if (current == null) return false
+        }
+
+        // 删除的结点是叶子结点
+        if (current.left == null && current.right == null) {
+            if (current == this.root) {
+                this.root = null
+            } else if (isLeftChild) {
+                current.left = null
+            } else {
+                current.right = null
+            }
+        }
+        // 删除的结点只有一个子结点
+        else if (current.left == null) {
+            if (current == this.root) {
+                this.root = current.right
+            } else if (isLeftChild) {
+                parent.left = current.right
+            } else {
+                parent.right = current.right
+            }
+        } else if (current.right == null) {
+            if (current == this.root) {
+                this.root = current.left
+            } else if (isLeftChild) {
+                parent.left = current.left
+            } else {
+                parent.right = current.left
+            }
+        }
+        // 删除的结点有两个子结点
+        else {
+            let successor = this.getSuccessor(current)
+
+            if (current == this.root) {
+                this.root = successor
+            } else if (isLeftChild) {
+                parent.left = successor
+            } else {
+                parent.right = successor
+            }
+
+            successor.left = current.left
+        }
+    }
+
+    BinarySearchTree.prototype.getSuccessor = function (delNode) {
+        let successor = delNode
+        let successorParent = delNode
+        let current = successor.right
+
+        while(current.right != null) {
+            successorParent = successor
+            successor = current
+            current = current.left
+        }
+
+        // 判断寻找的后继结点是否是直接右子节点
+        if (successor != delNode.right) {
+            successorParent.left = successor.right
+            successor.right = delNode.right
+        }
+
+        return successor
+    }
+}
+let bst = new BinarySearchTree()
+bst.insert(11)
+bst.insert(7)
+bst.insert(15)
+bst.insert(5)
+bst.insert(3)
+bst.insert(9)
+bst.insert(8)
+bst.insert(10)
+bst.insert(13)
+bst.insert(12)
+bst.insert(14)
+bst.insert(20)
+bst.insert(18)
+bst.insert(25)
+bst.insert(6)
+let res = ''
+bst.preOrderTraversal(function(key){
+    res += key+' '
+})
+console.log(res)
+res = ''
+bst.midOrderTraversal(function(key){
+    res += key+' '
+})
+console.log(res)
+res = ''
+bst.postOrderTraversal(function(key){
+    res += key+' '
+})
+console.log(res)
+console.log(bst.min())
+console.log(bst.max())
+console.log(bst.search(2))
+
+
+function BinarySearchTree() {
+    function Node(key) {
+        this.key = key
+        this.color = 'red'
+        this.left = null
+        this.right = null
+    }
+    this.root = null
+
+    BinarySearchTree.prototype.insertAvl = function (key) {
+        let newNode = new Node(key)
+        if (this.root == null) {
+            let newNode = new Node(key)
+            this.root = newNode
+            this.root.color = 'black'
+        } else {
+            this.insertAvlNode(this.root, newNode)
+        }
+    }
+
+    BinarySearchTree.prototype.situation = function (pnode, unode, gnode) {
+        if (pnode.color == 'red' && unode.color == 'red' && gnode.color == 'black') {
+            
+        }
+    }
+
+    BinarySearchTree.prototype.insertAvlNode = function (node, newNode) {
+        if (newNode.key < node.key) {
+            if (node.left == null ) {
+                if (node.color != 'red') {
+                    node.left = newNode
+                } else {
+
+                }
+                
+            } else {
+                this.insertNode(node.left, newNode)
+            }
+        } else {
+            if (node.right == null && node.color != 'red') {
+                node.right = newNode
+            } else {
+                this.insertNode(node.right, newNode)
+            }
+        }
+    }
+}
